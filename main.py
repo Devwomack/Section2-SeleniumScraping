27fe97cea61c6a1e93fa2fb3097315365c968a8d
devwo <119653518+Devwomack@users.noreply.github.com>
2023-01-26 22:19:13 +0000

    main now does login, navigate to HOME, then select and print cleaned text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
@@ -10,6 +11,8 @@ chrome_options.add_argument('--start-maximized')
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
+dwait = float(0.5) #default wait time in seconds 
+
 driver = webdriver.Chrome(options=chrome_options)
 
 def clean_text(text):
@@ -18,9 +21,19 @@ def clean_text(text):
   return output
 
 def main():
-  driver.get("http://automated.pythonanywhere.com") #load the page
-  time.sleep(2) #idle for 2 seconds, dynamic page elements have time to load
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)
+  driver.get("http://automated.pythonanywhere.com/login/") #load the page
+  #enter creds and press RETURN
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(dwait)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  #wait for load, then find & click the HOME button element
+  time.sleep(dwait)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  #wait for laod, then find the element with the desired text 
+  time.sleep(2)
+  mytext = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+  #clean up the text and return it
+  return clean_text(mytext.text)
 
 print(main())
+
